29191bcb7325252373a7283e5d32ba483ed4bf2a
Fabio Ricali <ricalifabio@gmail.com>
2019-07-09 10:12:42 +0200

    Update index.js

--- client/config/index.js
+++ client/config/index.js
@@ -4,7 +4,7 @@ import production from './production';
 let config = production;
 
 // The check is on location.href, in this case detect if is localhost
-if (/localhost/.test(window.location.href) || window.__DOZ_SSR_PATH__) {
+if (/localhost/.test(window.location.href) /*|| window.__DOZ_SSR_PATH__*/) {
     config = develop;
 }
 
